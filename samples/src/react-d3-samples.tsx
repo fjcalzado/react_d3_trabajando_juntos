51c9c9902288cc374d776ec40165cd071780e711
fjcalzado <fjcalzadorodriguez@gmail.com>
2018-05-06 13:37:23 +0200

    Fixed render dom issue

--- samples/src/react-d3-samples.tsx
+++ samples/src/react-d3-samples.tsx
@@ -5,22 +5,30 @@ import { HelloComponent } from './countries/hello';
 import { CollisionComponent } from './collision/collision.component';
 import { D3RenderComponent } from './d3-render/d3render.component';
 
-ReactDOM.render(
-  <HelloComponent/>,
-  document.getElementById('sample-countries')
-);
+if (document.getElementById('sample-countries')) {
+  ReactDOM.render(
+    <HelloComponent/>,
+    document.getElementById('sample-countries')
+  );
+}
 
-ReactDOM.render(
-  <CollisionComponent/>,
-  document.getElementById('sample-collision')
-);
+if (document.getElementById('sample-collision')) {
+  ReactDOM.render(
+    <CollisionComponent/>,
+    document.getElementById('sample-collision')
+  );
+}
 
-ReactDOM.render(
-  <D3RenderComponent dynamic={false} numBars={10} />,
-  document.getElementById('sample-d3-render-static')
-);
+if (document.getElementById('sample-d3-render-static')) {
+  ReactDOM.render(
+    <D3RenderComponent dynamic={false} numBars={10} />,
+    document.getElementById('sample-d3-render-static')
+  );
+}
 
-ReactDOM.render(
-  <D3RenderComponent dynamic={true} numBars={30} />,
-  document.getElementById('sample-d3-render-dynamic')
-)
+if (document.getElementById('sample-d3-render-dynamic')) {
+  ReactDOM.render(
+    <D3RenderComponent dynamic={true} numBars={30} />,
+    document.getElementById('sample-d3-render-dynamic')
+  );
+}
